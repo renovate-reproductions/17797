ee63022c558e9d6c1d10d3720324aeda9e5fc6ec
Karl Hepler <karl.hepler@gmail.com>
2022-09-26 17:03:46 -0400

    import something

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/karlhepler/renovate-test
 
 go 1.19
 
-require golang.org/x/pkgsite v0.0.0-20220921205104-d06df0691bf0 // indirect
+require golang.org/x/pkgsite v0.0.0-20220921205104-d06df0691bf0
--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 
 	// This is just here to prevent `go mod tidy` from
 	// removing the package.
-	_ "golang.org/x/pkgsite"
+	thirdparty "golang.org/x/pkgsite/third_party"
 )
 
 func main() {
-	fmt.Println("howdy")
+	fmt.Println(thirdparty.FS)
 }
